4c10e9657983b4aaff9855e7df3182bd428c0c04
vish-srivastava <vishal24000@gmail.com>
2023-07-24 17:59:38 +0530

    Initial Project Setup - Publicis Assessment

---
+++ src/main/java/com/recommendation/meal/MealRecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.recommendation.meal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealRecommendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealRecommendationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recommendation/meal/MealRecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recommendation.meal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealRecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
